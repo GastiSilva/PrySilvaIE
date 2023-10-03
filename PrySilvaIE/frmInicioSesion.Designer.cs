1232c7c4c8e2629de012441a2fb80503dd6264e2
GastiSilva <gasti.silva@gmail.com>
2023-10-03 15:45:26 -0300

    20230310 avanze con el inicio de sesion

---
+++ PrySilvaIE/frmInicioSesion.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PrySilvaIE
+{
+    partial class frmInicioSesion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicioSesion));
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarSesion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(179, 100);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(192, 42);
+            this.btnIniciarSesion.TabIndex = 0;
+            this.btnIniciarSesion.Text = "Iniciar Sesion";
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre de usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña:";
+            // 
+            // txtNombreUsuario
+            // 
+            this.txtNombreUsuario.Location = new System.Drawing.Point(179, 12);
+            this.txtNombreUsuario.Name = "txtNombreUsuario";
+            this.txtNombreUsuario.Size = new System.Drawing.Size(192, 24);
+            this.txtNombreUsuario.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(179, 57);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(192, 24);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // frmInicioSesion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(428, 162);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtNombreUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIniciarSesion);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmInicioSesion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio de sesion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+    }
+}
---
+++ PrySilvaIE/frmInicioSesion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrySilvaIE
+{
+    public partial class frmInicioSesion : Form
+    {
+        public frmInicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private int IntentosFallidos = 0;
+        private const int maxIntentosFallidos = 3;
+        private bool UsuarioBloqueado = false;
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            if (UsuarioBloqueado)
+            {
+                MessageBox.Show("Usuario bloqueado. Contacta al administrador.");
+                return;
+            }
+
+            string nombreUsuario = txtNombreUsuario.Text;
+            string contraseña = txtContraseña.Text;
+
+            // Consulta las credenciales (consulta la base de datos o estructura de datos)
+            if (ValidarCredenciales(nombreUsuario, contraseña))
+            {
+                IntentosFallidos = 0;
+                MessageBox.Show("Inicio de sesión exitoso.");
+                // abrir forms de carga
+            }
+            else
+            {
+                IntentosFallidos++;
+                if (IntentosFallidos >= maxIntentosFallidos)
+                {
+                    UsuarioBloqueado = true;
+                    MessageBox.Show("Usuario bloqueado después de 3 intentos fallidos.");
+                    BloquearUsuario();
+                }
+                else
+                {
+                    MessageBox.Show($"Nombre de usuario o contraseña incorrectos. Intentos fallidos: {IntentosFallidos}");
+                }
+            }
+
+        }
+
+        private void BloquearUsuario()
+        {
+            btnIniciarSesion.Enabled = false;
+        }
+        
+        private bool ValidarCredenciales(string nombreUsuario, string contraseña)
+        {
+            return nombreUsuario == "usuario" && contraseña == "contraseña";
+        }
+           
+        }
+}
